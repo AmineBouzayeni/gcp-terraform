5c5d1ecc87f669c47f94a69c4c3662429673143b
Amine Bouzayeni <mohamed-amine.bouzayeni@teolia.fr>
2023-06-14 16:59:35 +0200

    Add controller-vm automation

--- main.tf
+++ main.tf
@@ -1,5 +1,39 @@
+# Create Jenkins firewall rule 
+## TODOs: - Add project name
+resource "google_compute_firewall" "jenkins" {
+  project     = ""
+  name        = "jenkins"
+  network     = "default"
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["8080"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["jenkins"]
+}
+# Create train app firewall rule #
+## TODOs: - Add project name
+resource "google_compute_firewall" "train-app" {
+  project     = ""
+  name        = "train-app"
+  network     = "default"
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["3000"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["train"]
+}
+
 # Create a single Compute Engine instance
-resource "google_compute_instance" "default" {
+## TODOs: - Add project name
+resource "google_compute_instance" "jenkins-vm" {
   name         = "jenkins-vm"
   machine_type = "e2-medium"
   zone         = "europe-west1-b"
@@ -11,8 +45,89 @@ resource "google_compute_instance" "default" {
     }
   }
 
-  # Install Flask
+  # Install Jenkins
   metadata_startup_script = "set -xe; sudo yum -y install wget; sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo; sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key; sudo yum -y upgrade; sudo yum -y install java-11-openjdk; sudo yum -y install jenkins; sudo systemctl daemon-reload; sudo systemctl start jenkins"
+
+  network_interface {
+    subnetwork = "default"
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
+
+# Create a production server
+## TODOs: - Add project name
+##        - Install ansible agent
+resource "google_compute_instance" "staging-vm" {
+  name         = "staging-vm"
+  machine_type = "e2-medium"
+  zone         = "europe-west1-b"
+  tags         = ["ssh", "train"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-7"
+    }
+  }
+
+  # Install Ansible agent
+  metadata_startup_script = ""
+
+  network_interface {
+    subnetwork = "default"
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
+
+#Create a production vm
+## TODOs: - Add project name
+##        - Install ansible agent
+resource "google_compute_instance" "production-vm" {
+  name         = "production-vm"
+  machine_type = "e2-medium"
+  zone         = "europe-west1-b"
+  tags         = ["ssh", "app"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-7"
+    }
+  }
+
+  # Install Ansible agent
+  metadata_startup_script = ""
+
+  network_interface {
+    subnetwork = "default"
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
+
+#Create an Ansible controller vm
+## TODOs: - Add project name
+##        - Add startup metadata script steps to install ansible controller
+resource "google_compute_instance" "ansible-vm" {
+  name         = "ansible-vm"
+  machine_type = "e2-medium"
+  zone         = "europe-west1-b"
+  tags         = ["ssh", "train"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-7"
+    }
+  }
+
+  # Install Ansible controller
+  metadata_startup_script = "sudo yum install ansible -y; sudo echo [ansible_client]; sudo ${google_compute_instance.network_interface.0.access_config.0.nat_ip} ansible_ssh_user=cloud_user ansible_ssh_pass=TODO"
 
   network_interface {
     subnetwork = "default"
